3c1e41e1744b07c9de46b97eea195e2cf6a5c59d
Jonah Snider <jonah@jonah.pw>
2019-12-01 14:08:37 -0800

    fix: add return type to function
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
  * @param subject What the subject of the message is.
  * @returns Hello world message
  */
-export function helloWorld(subject?: string) {
+export function helloWorld(subject?: string): string {
 	return `Hello, ${subject ?? 'world'}.`;
 }
