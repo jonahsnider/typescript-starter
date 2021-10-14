deb992a462c50146df3e2d092bce6c5986013e66
Jonah Snider <jonah@jonahsnider.com>
2021-10-14 01:38:40 -0700

    docs: fix TSDoc comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Says hello world.
- * @param subject What the subject of the message is.
+ * @param subject - What the subject of the message is.
  * @returns Hello world message
  */
 export function helloWorld(subject?: string): string {
